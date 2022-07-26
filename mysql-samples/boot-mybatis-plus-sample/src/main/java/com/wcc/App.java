3699d0b3a5d81a6aa19c2af1db4b6ba2242bc988
Jiangtao.Wu <cqu20141693@gmail.com>
2022-07-26 19:07:03 +0800

    add mybatis-plus demo

---
+++ mysql-samples/boot-mybatis-plus-sample/src/main/java/com/wcc/App.java
@@ -0,0 +1,23 @@
+package com.wcc;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * wcc 2022/7/26
+ */
+@SpringBootApplication
+@Slf4j
+public class App implements CommandLineRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("mybatis plus start");
+    }
+}
